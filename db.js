2352d2fc52c807d377a4c15588412a970b6d0ebb
inverse9 <permanawahyu97@gmail.com>
2024-12-04 20:16:43 +0800

    database password env prod

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "M|qNz{K4;~$&",
   database: "qrcode_capstone",
 });
 
